06cf0ba7501e4b0516f69766730682f24a732b45
Sumin Byeon <suminb@gmail.com>
2017-06-15 01:20:32 +0900

    Take a description as a parameter

--- auction/__main__.py
+++ auction/__main__.py
@@ -25,16 +25,18 @@ def drop_all():
 
 @cli.command()
 @click.argument('title')
-def make_auction(title):
+@click.argument('description')
+def make_auction(title, description):
     with create_app(__name__).app_context():
         now = datetime.utcnow()
         auction = Auction.create(
             title=title,
+            description=description,
             starts_at=now,
             ends_at=now + timedelta(minutes=30),
         )
 
-        log.info('{} has been created.', auction)
+        log.info('Auction-{} has been created.', auction.id)
 
 
 if __name__ == '__main__':
