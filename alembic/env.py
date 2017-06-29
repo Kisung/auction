dcf8989326d6297483992084c6629de45a4667be
Sumin Byeon <suminb@gmail.com>
2017-06-29 06:54:37 +0000

    Read an environment variable to set Alembic DB URL

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,9 @@
 from __future__ import with_statement
+
+import os
+
 from alembic import context
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine
 from logging.config import fileConfig
 
 # this is the Alembic Config object, which provides
@@ -23,6 +26,10 @@ target_metadata = None
 # ... etc.
 
 
+def get_url():
+    return os.environ['DB_URL']
+
+
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
@@ -35,7 +42,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True)
 
@@ -50,10 +57,11 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix='sqlalchemy.',
-        poolclass=pool.NullPool)
+    # connectable = engine_from_config(
+    #     config.get_section(config.config_ini_section),
+    #     prefix='sqlalchemy.',
+    #     poolclass=pool.NullPool)
+    connectable = create_engine(get_url())
 
     with connectable.connect() as connection:
         context.configure(
