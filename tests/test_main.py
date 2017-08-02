c65f66f6c8b976fe8f6fd247da5f793c33b9f909
Sumin Byeon <suminb@gmail.com>
2017-08-02 23:30:12 +0900

    Write a test case for creating bids

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,3 +5,20 @@ def test_view_auction(testapp, make_auction):
     auction = make_auction()
     resp = testapp.get('/auctions/{0}'.format(auction.id))
     assert resp.status_code == 200
+
+
+def test_create_bid(testapp, make_auction):
+    auction = make_auction()
+    url = '/bids?auction_id={0}'.format(auction.id)
+
+    resp = testapp.get(url)
+    assert resp.status_code == 200
+
+    data = {
+        'name': 'Name',
+        'email': 'test@test.com',
+        'price': 1000,
+        'consent': True,
+    }
+    resp = testapp.post(url, data=data)
+    assert resp.status_code == 302  # redirection upon validation
